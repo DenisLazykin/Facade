3dc94b108c52d9b9a0ff3cd811134fc0f1d23296
Denis Lazykin <dlazykin856@gmail.com>
2020-12-07 11:18:06 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        String arg1 = "11";
+        String arg2 = "110";
+        System.out.println(bins.sum(arg1, arg2));
+        System.out.println(bins.mult(arg1, arg2));
+    }
+}
